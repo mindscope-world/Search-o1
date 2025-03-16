f34666e4e19f2f64841e70a7ca28c21ccba0e06e
Paul Ndirangu <ndirangup254@gmail.com>
2025-03-16 20:19:38 +0300

    Modified Bing-search to use DuckduckGo

--- scripts/run_search_o1.py
+++ scripts/run_search_o1.py
@@ -285,6 +285,7 @@ def main():
         model=model_path,
         tensor_parallel_size=torch.cuda.device_count(),
         gpu_memory_utilization=0.95,
+        dtype='half'
     )
 
     # ---------------------- Data Loading ----------------------
